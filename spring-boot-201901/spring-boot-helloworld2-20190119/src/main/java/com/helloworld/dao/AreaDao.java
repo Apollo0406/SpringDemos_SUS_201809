3c9c391da97f77300ff00c8a6cd601da4c64a84a
Apollo0406 <2433823963@qq.com>
2019-01-19 22:28:57 +0800

    finished service

---
+++ spring-boot-201901/spring-boot-helloworld2-20190119/src/main/java/com/helloworld/dao/AreaDao.java
@@ -0,0 +1,18 @@
+package com.helloworld.dao;
+
+import com.helloworld.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    //列出整个区域列表
+    List<Area> queryArea();
+    //按照id来查询区域
+    Area queryAreaById(int areaId);
+    //插入区域
+    int insertArea(Area area);
+    //修改区域
+    void updateArea(Area area);
+    //删除区域
+    int deleteArea(int areaId);
+}
